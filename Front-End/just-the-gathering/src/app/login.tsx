9a569199fe36020b7a68578930e97303ea1aa62c
Mark Jensen <mark.jensen@fhnw.edu>
2025-05-13 14:00:57 -0600

    added basic sign in

--- Front-End/just-the-gathering/src/app/login.tsx
+++ Front-End/just-the-gathering/src/app/login.tsx
@@ -1,13 +1,16 @@
-"use client"
-import styles from "./page.module.css";
-
-
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-       
-       </main>
-    </div>
-  );
+import styles from "./page.module.css"
+type UserProps = {
+  first: string
+  last: string
 }
+
+export default function User({first, last}: UserProps){
+  return (
+    <div className="login">
+<h2>{first} {last}</h2>
+</div>
+  )
+}
+
+
+// UPDATE `theGathering` SET `Name`='[value-1]',`Password`='[value-2]',`meetingLocation`='[value-3]',`meetingTime`='[value-4]' WHERE 1
--- gatheringXcode/theGathering/pages/CreatePage.swift
+++ gatheringXcode/theGathering/pages/CreatePage.swift
@@ -1,18 +1,18 @@
 //
-//  gameScreen.swift
+//  CreatePage.swift
 //  theGathering
 //
-//  Created by Mark Jensen on 5/12/25.
+//  Created by Mark Jensen on 5/13/25.
 //
 
 import SwiftUI
 
-struct gameScreen: View {
+struct CreatePage: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    gameScreen()
+    CreatePage()
 }
